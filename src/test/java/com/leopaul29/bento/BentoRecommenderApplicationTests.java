4fad450fc6ca6907450a1395e532766ef69ea379
leopaul29 <leopaulmartin.iut@gmail.com>
2025-07-31 23:26:58 +0900

    init project with start.spring.io archive and docker files

---
+++ src/test/java/com/leopaul29/bento/BentoRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leopaul29.bento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BentoRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
