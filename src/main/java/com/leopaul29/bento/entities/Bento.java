c3c471da01777b42107ebe45f4772adacc958adf
leopaul29 <leopaulmartin.iut@gmail.com>
2025-08-02 16:53:30 +0900

    update entity with lombock

--- src/main/java/com/leopaul29/bento/entities/Bento.java
+++ src/main/java/com/leopaul29/bento/entities/Bento.java
@@ -1,23 +1,29 @@
 package com.leopaul29.bento.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Objects;
 import java.util.Set;
 
-@Setter
-@Getter
+
 @Entity
 @Table(name = "bentos")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Bento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String description;
+
     private int calorie;
+
     @ManyToMany
     @JoinTable(
             name = "bento_ingredient",
--- src/main/java/com/leopaul29/bento/entities/Ingredient.java
+++ src/main/java/com/leopaul29/bento/entities/Ingredient.java
@@ -1,23 +1,22 @@
 package com.leopaul29.bento.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Objects;
 
-@Setter
-@Getter
-@AllArgsConstructor
+
 @Entity
+@Table(name = "ingredients")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Ingredient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
     @Override
