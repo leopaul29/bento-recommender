2905f718f49c0438364247f25d4a57c2adfb1c8a
leopaul29 <leopaulmartin.iut@gmail.com>
2025-08-03 18:07:12 +0900

    fix User entity and repository + init data

--- src/main/java/com/leopaul29/bento/entities/User.java
+++ src/main/java/com/leopaul29/bento/entities/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -17,15 +18,36 @@ public class User {
     @ManyToMany
     @JoinTable(
             name = "user_disliked_ingredient",
-            joinColumns = @JoinColumn(name = "user_pref_id"),
+            joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "ingredient_id")
     )
     private Set<Ingredient> dislikedIngredients;
     @ManyToMany
     @JoinTable(
             name = "user_liked_tag",
-            joinColumns = @JoinColumn(name = "user_pref_id"),
+            joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "tag_id")
     )
     private Set<Tag> likedTags;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", dislikedIngredients=" + dislikedIngredients +
+                ", likedTags=" + likedTags +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(dislikedIngredients, user.dislikedIngredients) && Objects.equals(likedTags, user.likedTags);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, dislikedIngredients, likedTags);
+    }
 }
